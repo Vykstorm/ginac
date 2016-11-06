Vykstorm/ginac

commit 8305ec388e5c205b7392566d32582f9d08adb88d

Replace mktemp with mkstemp.
As the manpage says, mktemp is deprecated ("Never use mktemp") and gcc
warns about the usage of mktemp. Replace mktemp with its safe variant
mkstemp.

ginac/excompiler.cpp
@@ -38,6 +38,8 @@
 #ifdef HAVE_LIBDL
 #include <dlfcn.h>
 #endif // def HAVE_LIBDL
+#include <unistd.h>
+#include <stdlib.h>
 #include <fstream>
 #include <ios>
 #include <sstream>
@@ -48,7 +50,7 @@
 namespace GiNaC {
 
 #ifdef HAVE_LIBDL
-	
+
 /**
  * Small class that manages modules opened by libdl. It is used by compile_ex
  * and link_ex in order to have a clean-up of opened modules and their
@@ -112,12 +114,14 @@ public:
 			const char* filename_pattern = "./GiNaCXXXXXX";
 			char* new_filename = new char[strlen(filename_pattern)+1];
 			strcpy(new_filename, filename_pattern);
-			if (!mktemp(new_filename)) {
+			int fd = mkstemp(new_filename);
+			if (fd == -1) {
 				delete[] new_filename;
-				throw std::runtime_error("mktemp failed");
+				throw std::runtime_error("mkstemp failed");
 			}
 			filename = std::string(new_filename);
 			ofs.open(new_filename, std::ios::out);
+			close(fd);
 			delete[] new_filename;
 		} else {
 			// use parameter as filename
@@ -155,7 +159,7 @@ public:
 	{
 		void* module = nullptr;
 		module = dlopen(filename.c_str(), RTLD_NOW);
-		if (module == nullptr)	{
+		if (module == nullptr) {
 			throw std::runtime_error("excompiler::link_so_file: could not open compiled module!");
 		}
 

commit 864d9d65c5ecd0b4fffa9162c156383fd5cb9951

Clean up some header files and fix compilation with MSC.
On MSC there is no <unistd.h>. It turns out that we need to #include <io.h>
in order to use close(3). This fixes the build failure introduced in 8305ec38.

Thanks to Jan Rheinländer for reporting this.

acinclude.m4
@@ -34,7 +34,6 @@ AC_CACHE_CHECK([for standard C++ header files], [ginac_cv_std_cxx_headers], [
 		#include <cstdio>
 		#include <cstdlib>
 		#include <cstring>
-		#include <cstddef>
 		#include <cstdint>
 		#include <ctime>
 		#include <fstream>

check/mul_eval_memleak.cpp
@@ -96,7 +96,6 @@ g(1301*scale^(-1)))*eps^(-1)+F))\
 #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/resource.h>
-#include <unistd.h>
 
 static void set_VM_limit(const unsigned long MB) {
 	const unsigned mem_lim_kb = MB*1024*1024;

check/timer.cpp
@@ -22,7 +22,6 @@
 
 #ifdef HAVE_RUSAGE
 #include <sys/resource.h>
-#include <unistd.h>
 #include <sys/time.h>
 #else
 #include <ctime>

ginac/excompiler.cpp
@@ -36,10 +36,16 @@
 #include "symbol.h"
 
 #ifdef HAVE_LIBDL
-#include <dlfcn.h>
+# include <dlfcn.h>
 #endif // def HAVE_LIBDL
-#include <unistd.h>
-#include <stdlib.h>
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#else
+# ifdef _MSC_VER
+#  include <io.h>  // for close(3)
+# endif // def _MSC_VER
+#endif // def HAVE_UNISTD_H
+#include <cstdlib>
 #include <fstream>
 #include <ios>
 #include <sstream>