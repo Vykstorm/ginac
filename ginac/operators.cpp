Vykstorm/ginac

commit 82df718524319471d3a92fb051329aa8cd529c22

Merge some cosmetic patches.
This is a collection of some minor optimizations and indentation fixes.

ginac/operators.cpp
@@ -34,13 +34,13 @@
 
 namespace GiNaC {
 
-/** Used internally by operator+() to add two ex objects together. */
+/** Used internally by operator+() to add two ex objects. */
 static inline const ex exadd(const ex & lh, const ex & rh)
 {
 	return dynallocate<add>(lh, rh);
 }
 
-/** Used internally by operator*() to multiply two ex objects together. */
+/** Used internally by operator*() to multiply two ex objects. */
 static inline const ex exmul(const ex & lh, const ex & rh)
 {
 	// Check if we are constructing a mul object or a ncmul object.  Due to
@@ -247,32 +247,32 @@ const numeric operator--(numeric & lh, int)
 
 const relational operator==(const ex & lh, const ex & rh)
 {
-	return relational(lh,rh,relational::equal);
+	return dynallocate<relational>(lh, rh, relational::equal);
 }
 
 const relational operator!=(const ex & lh, const ex & rh)
 {
-	return relational(lh,rh,relational::not_equal);
+	return dynallocate<relational>(lh, rh, relational::not_equal);
 }
 
 const relational operator<(const ex & lh, const ex & rh)
 {
-	return relational(lh,rh,relational::less);
+	return dynallocate<relational>(lh, rh, relational::less);
 }
 
 const relational operator<=(const ex & lh, const ex & rh)
 {
-	return relational(lh,rh,relational::less_or_equal);
+	return dynallocate<relational>(lh, rh, relational::less_or_equal);
 }
 
 const relational operator>(const ex & lh, const ex & rh)
 {
-	return relational(lh,rh,relational::greater);
+	return dynallocate<relational>(lh, rh, relational::greater);
 }
 
 const relational operator>=(const ex & lh, const ex & rh)
 {
-	return relational(lh,rh,relational::greater_or_equal);
+	return dynallocate<relational>(lh, rh, relational::greater_or_equal);
 }
 
 // input/output stream operators and manipulators

INSTALL
@@ -5,7 +5,7 @@ GiNaC requires the CLN library by Bruno Haible installed on your system.
 It is available from <ftp://ftpthep.physik.uni-mainz.de/pub/gnu/>.
 
 You will also need a decent ISO C++-11 compiler. We recommend the C++
-compiler from the GNU compiler collection, GCC >= 4.9. If you have a
+compiler from the GNU compiler collection, GCC >= 4.8. If you have a
 different or older compiler you are on your own. Note that you may have to
 use the same compiler you compiled CLN with because of differing
 name-mangling schemes.
@@ -20,7 +20,7 @@ TeX are necessary.
 
 Known to work with:
  - Linux on x86 and x86_64 using 
-   - GCC 4.8, 4.9, 5.1, and 5.2
+   - GCC 4.8, 4.9, 5.1, 5.2, and 5.3
    - Clang 3.5.0
 
 Known not to work with:

doc/CodingStyle
@@ -27,7 +27,7 @@ please try to follow these rules. It will make our (and your) lives easier. :-)
 ----------------------
 
 Any code in GiNaC should comply to the C++ standard, as defined by ISO/IEC
-14882:1998(E). Don't use compiler-specific language extensions unless they
+14882:2011(E). Don't use compiler-specific language extensions unless they
 are surrounded by appropriate "#ifdef"s, and you also provide a compliant
 version of the code in question for other compilers.
 

ginac/color.cpp
@@ -400,9 +400,7 @@ bool su3d::contract_with(exvector::iterator self, exvector::iterator other, exve
 		 && ex_to<indexed>(*self).has_dummy_index_for(other[1].op(1))) {
 
 			exvector self_indices = ex_to<indexed>(*self).get_indices();
-			exvector dummy_indices;
-			dummy_indices.push_back(other[0].op(1));
-			dummy_indices.push_back(other[1].op(1));
+			exvector dummy_indices = {other[0].op(1), other[1].op(1)};
 			int sig;
 			ex a = permute_free_index_to_front(self_indices, dummy_indices, sig);
 			*self = numeric(5, 6);
@@ -453,9 +451,7 @@ bool su3f::contract_with(exvector::iterator self, exvector::iterator other, exve
 		 && ex_to<indexed>(*self).has_dummy_index_for(other[1].op(1))) {
 
 			exvector self_indices = ex_to<indexed>(*self).get_indices();
-			exvector dummy_indices;
-			dummy_indices.push_back(other[0].op(1));
-			dummy_indices.push_back(other[1].op(1));
+			exvector dummy_indices = {other[0].op(1), other[1].op(1)};
 			int sig;
 			ex a = permute_free_index_to_front(self_indices, dummy_indices, sig);
 			*self = numeric(3, 2) * sig * I;

ginac/expairseq.cpp
@@ -601,11 +601,8 @@ bool expairseq::can_make_flat(const expair &p) const
 
 void expairseq::construct_from_2_ex_via_exvector(const ex &lh, const ex &rh)
 {
-	exvector v;
-	v.reserve(2);
-	v.push_back(lh);
-	v.push_back(rh);
-	construct_from_exvector(v);
+	const exvector v = {lh, rh};
+	construct_from_exvector(std::move(v));
 }
 
 void expairseq::construct_from_2_ex(const ex &lh, const ex &rh)

ginac/expairseq.h
@@ -166,7 +166,7 @@ class make_flat_inserter
 				return x;
 			sort(dummies_of_factor.begin(), dummies_of_factor.end(), ex_is_less());
 			ex new_factor = rename_dummy_indices_uniquely(used_indices,
-				dummies_of_factor, x);
+			                                              dummies_of_factor, x);
 			combine_indices(dummies_of_factor);
 			return new_factor;
 		}
@@ -175,8 +175,8 @@ class make_flat_inserter
 		{
 			exvector new_dummy_indices;
 			set_union(used_indices.begin(), used_indices.end(),
-				dummies_of_factor.begin(), dummies_of_factor.end(),
-				std::back_insert_iterator<exvector>(new_dummy_indices), ex_is_less());
+			          dummies_of_factor.begin(), dummies_of_factor.end(),
+			          std::back_insert_iterator<exvector>(new_dummy_indices), ex_is_less());
 			used_indices.swap(new_dummy_indices);
 		}
 		bool do_renaming;

ginac/factor.cpp
@@ -916,8 +916,7 @@ static void berlekamp(const umodpoly& a, upvec& upv)
 		return;
 	}
 
-	list<umodpoly> factors;
-	factors.push_back(a);
+	list<umodpoly> factors = {a};
 	unsigned int size = 1;
 	unsigned int r = 1;
 	unsigned int q = cl_I_to_uint(R->modulus);
@@ -937,8 +936,7 @@ static void berlekamp(const umodpoly& a, upvec& upv)
 				div(*u, g, uo);
 				if ( equal_one(uo) ) {
 					throw logic_error("berlekamp: unexpected divisor.");
-				}
-				else {
+				} else {
 					*u = uo;
 				}
 				factors.push_back(g);
@@ -1021,8 +1019,7 @@ static void modsqrfree(const umodpoly& a, upvec& factors, vector<int>& mult)
 				mult[i] *= prime;
 			}
 		}
-	}
-	else {
+	} else {
 		umodpoly ap;
 		expt_1_over_p(a, prime, ap);
 		size_t previ = mult.size();
@@ -1107,8 +1104,7 @@ static void same_degree_factor(const umodpoly& a, upvec& upv)
 	for ( size_t i=0; i<degrees.size(); ++i ) {
 		if ( degrees[i] == degree(ddfactors[i]) ) {
 			upv.push_back(ddfactors[i]);
-		}
-		else {
+		} else {
 			berlekamp(ddfactors[i], upv);
 		}
 	}
@@ -1314,8 +1310,7 @@ static void hensel_univar(const upoly& a_, unsigned int p, const umodpoly& u1_, 
 		if ( alpha != 1 ) {
 			w = w / alpha;
 		}
-	}
-	else {
+	} else {
 		u.clear();
 	}
 }
@@ -1328,20 +1323,22 @@ static void hensel_univar(const upoly& a_, unsigned int p, const umodpoly& u1_, 
 static unsigned int next_prime(unsigned int p)
 {
 	static vector<unsigned int> primes;
-	if ( primes.size() == 0 ) {
-		primes.push_back(3); primes.push_back(5); primes.push_back(7);
+	if (primes.empty()) {
+		primes = {3, 5, 7};
 	}
 	if ( p >= primes.back() ) {
 		unsigned int candidate = primes.back() + 2;
 		while ( true ) {
 			size_t n = primes.size()/2;
 			for ( size_t i=0; i<n; ++i ) {
-				if ( candidate % primes[i] ) continue;
+				if (candidate % primes[i])
+					continue;
 				candidate += 2;
 				i=-1;
 			}
 			primes.push_back(candidate);
-			if ( candidate > p ) break;
+			if (candidate > p)
+				break;
 		}
 		return candidate;
 	}
@@ -1405,8 +1402,7 @@ public:
 			if ( len > n/2 ) return false;
 			fill(k.begin(), k.begin()+len, 1);
 			fill(k.begin()+len+1, k.end(), 0);
-		}
-		else {
+		} else {
 			k[last++] = 0;
 			k[last] = 1;
 		}
@@ -1429,8 +1425,7 @@ private:
 			if ( d ) {
 				if ( cache[pos].size() >= d ) {
 					lr[group] = lr[group] * cache[pos][d-1];
-				}
-				else {
+				} else {
 					if ( cache[pos].size() == 0 ) {
 						cache[pos].push_back(factors[pos] * factors[pos+1]);
 					}
@@ -1444,8 +1439,7 @@ private:
 					}
 					lr[group] = lr[group] * cache[pos].back();
 				}
-			}
-			else {
+			} else {
 				lr[group] = lr[group] * factors[pos];
 			}
 		} while ( i < n );
@@ -1456,8 +1450,7 @@ private:
 		lr[1] = one;
 		if ( n > 6 ) {
 			split_cached();
-		}
-		else {
+		} else {
 			for ( size_t i=0; i<n; ++i ) {
 				lr[k[i]] = lr[k[i]] * factors[i];
 			}
@@ -1542,8 +1535,7 @@ static ex factor_univariate(const ex& poly, const ex& x, unsigned int& prime)
 			minfactors = trialfactors.size();
 			lastp = prime;
 			trials = 1;
-		}
-		else {
+		} else {
 			++trials;
 		}
 	}
@@ -1597,15 +1589,13 @@ static ex factor_univariate(const ex& poly, const ex& x, unsigned int& prime)
 						}
 					}
 					break;
-				}
-				else {
+				} else {
 					upvec newfactors1(part.size_left()), newfactors2(part.size_right());
 					auto i1 = newfactors1.begin(), i2 = newfactors2.begin();
 					for ( size_t i=0; i<n; ++i ) {
 						if ( part[i] ) {
 							*i2++ = tocheck.top().factors[i];
-						}
-						else {
+						} else {
 							*i1++ = tocheck.top().factors[i];
 						}
 					}
@@ -1617,8 +1607,7 @@ static ex factor_univariate(const ex& poly, const ex& x, unsigned int& prime)
 					tocheck.push(mf);
 					break;
 				}
-			}
-			else {
+			} else {
 				// not successful
 				if ( !part.next() ) {
 					// if no more combinations left, return polynomial as
@@ -1808,8 +1797,7 @@ static upvec univar_diophant(const upvec& a, const ex& x, unsigned int m, unsign
 			rem(bmod, a[j], buf);
 			result.push_back(buf);
 		}
-	}
-	else {
+	} else {
 		umodpoly s, t;
 		eea_lift(a[1], a[0], x, p, k, s, t);
 		umodpoly bmod = umodpoly_to_umodpoly(s, R, m);
@@ -1843,8 +1831,7 @@ struct make_modular_map : public map_function {
 			numeric n(R->retract(emod));
 			if ( n > halfmod ) {
 				return n-mod;
-			}
-			else {
+			} else {
 				return n;
 			}
 		}
@@ -1937,8 +1924,7 @@ static vector<ex> multivar_diophant(const vector<ex>& a_, const ex& x, const ex&
 				e = make_modular(buf, R);
 			}
 		}
-	}
-	else {
+	} else {
 		upvec amod;
 		for ( size_t i=0; i<a.size(); ++i ) {
 			umodpoly up;
@@ -1952,8 +1938,7 @@ static vector<ex> multivar_diophant(const vector<ex>& a_, const ex& x, const ex&
 		if ( is_a<add>(c) ) {
 			nterms = c.nops();
 			z = c.op(0);
-		}
-		else {
+		} else {
 			nterms = 1;
 			z = c;
 		}
@@ -2090,8 +2075,7 @@ static ex hensel_multivar(const ex& a, const ex& x, const vector<EvalPoint>& I,
 			res.append(U[i]);
 		}
 		return res;
-	}
-	else {
+	} else {
 		lst res;
 		return lst{};
 	}
@@ -2325,8 +2309,7 @@ static ex factor_multivariate(const ex& poly, const exset& syms)
 			for ( size_t i=1; i<ufaclst.nops(); ++i ) {
 				C[i-1] = ufaclst.op(i).lcoeff(x);
 			}
-		}
-		else {
+		} else {
 			// difficult case.
 			// we use the property of the ftilde having a unique prime factor.
 			// details can be found in [Wan].
@@ -2361,8 +2344,7 @@ static ex factor_multivariate(const ex& poly, const exset& syms)
 					}
 					C[i] = D[i] * prefac;
 				}
-			}
-			else {
+			} else {
 				for ( int i=0; i<factor_count; ++i ) {
 					numeric prefac = ex_to<numeric>(ufaclst.op(i+1).lcoeff(x));
 					for ( int j=ftilde.size()-1; j>=0; --j ) {
@@ -2486,8 +2468,7 @@ static ex factor_sqrfree(const ex& poly)
 			int ld = poly.ldegree(x);
 			ex res = factor_univariate(expand(poly/pow(x, ld)), x);
 			return res * pow(x,ld);
-		}
-		else {
+		} else {
 			ex res = factor_univariate(poly, x);
 			return res;
 		}
@@ -2514,8 +2495,7 @@ struct apply_factor_map : public map_function {
 			for ( size_t i=0; i<e.nops(); ++i ) {
 				if ( e.op(i).info(info_flags::polynomial) ) {
 					s1 += e.op(i);
-				}
-				else {
+				} else {
 					s2 += e.op(i);
 				}
 			}
@@ -2577,17 +2557,14 @@ ex factor(const ex& poly, unsigned options)
 				const ex& base = t.op(0);
 				if ( !is_a<add>(base) ) {
 					res *= t;
-				}
-				else {
+				} else {
 					ex f = factor_sqrfree(base);
 					res *= pow(f, t.op(1));
 				}
-			}
-			else if ( is_a<add>(t) ) {
+			} else if ( is_a<add>(t) ) {
 				ex f = factor_sqrfree(t);
 				res *= f;
-			}
-			else {
+			} else {
 				res *= t;
 			}
 		}

ginac/function.cppy
@@ -142,7 +142,7 @@ function_options & function_options::set_return_type(unsigned rt, const return_t
 {
 	use_return_type = true;
 	return_type = rt;
-	if (rtt != 0)
+	if (rtt != nullptr)
 		return_type_tinfo = *rtt;
 	else
 		return_type_tinfo = make_return_type_t<function>();
@@ -380,7 +380,7 @@ ex function::eval() const
 			// Something has changed while sorting arguments, more evaluations later
 			if (sig == 0)
 				return _ex0;
-			return ex(sig) * thiscontainer(v);
+			return ex(sig) * thiscontainer(std::move(v));
 		}
 	}
 

commit ac0ef893e48af99dc632adcdf21edd6a402327b3

Fix memory leaks in operators returning relationals.
These memory leaks were introduced in 82df71852.

Thanks to Thomas Luthe for reporting this.

ginac/operators.cpp
@@ -247,32 +247,32 @@ const numeric operator--(numeric & lh, int)
 
 const relational operator==(const ex & lh, const ex & rh)
 {
-	return dynallocate<relational>(lh, rh, relational::equal);
+	return relational(lh, rh, relational::equal);
 }
 
 const relational operator!=(const ex & lh, const ex & rh)
 {
-	return dynallocate<relational>(lh, rh, relational::not_equal);
+	return relational(lh, rh, relational::not_equal);
 }
 
 const relational operator<(const ex & lh, const ex & rh)
 {
-	return dynallocate<relational>(lh, rh, relational::less);
+	return relational(lh, rh, relational::less);
 }
 
 const relational operator<=(const ex & lh, const ex & rh)
 {
-	return dynallocate<relational>(lh, rh, relational::less_or_equal);
+	return relational(lh, rh, relational::less_or_equal);
 }
 
 const relational operator>(const ex & lh, const ex & rh)
 {
-	return dynallocate<relational>(lh, rh, relational::greater);
+	return relational(lh, rh, relational::greater);
 }
 
 const relational operator>=(const ex & lh, const ex & rh)
 {
-	return dynallocate<relational>(lh, rh, relational::greater_or_equal);
+	return relational(lh, rh, relational::greater_or_equal);
 }
 
 // input/output stream operators and manipulators